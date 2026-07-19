27268ca673f70a7f7a78ebd3eaa5f119588c8f62
Vegctrp <kem.altair@gmail.com>
2026-07-19 10:51:16 +0900

    fix parser

--- eu4_achievements_manager/eu4_wiki_parser.py
+++ eu4_achievements_manager/eu4_wiki_parser.py
@@ -1,7 +1,21 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+def parse_wiki_achievement_table(html_raw_text):
+    soup = BeautifulSoup(html_raw_text, "html.parser")
+    BASE_URL = "https://eu4.paradoxwikis.com"
 
-def parse_achievement_table(table):
+    # img
+    for img in soup.find_all("img"):
+        if img.has_attr("src"):
+            img["src"] = urljoin(BASE_URL, img["src"])
+
+    # a
+    for a in soup.find_all("a"):
+        if a.has_attr("href"):
+            a["href"] = urljoin(BASE_URL, a["href"])
+
+    table = soup.find("table")
     achievements = []
 
     rows = table.find_all("tr")[1:]
@@ -11,6 +25,12 @@ def parse_achievement_table(table):
 
         if len(cols) != 7:
             continue
+
+        achievement_col = cols[0]
+        title_div = achievement_col.select_one(
+            "div[style*='font-weight']"
+        )
+        title = title_div.get_text(" ", strip=True)
 
         achievement_html = cols[0].decode_contents()
         starting_condition_html = cols[1].decode_contents()
@@ -22,6 +42,7 @@ def parse_achievement_table(table):
 
         achievements.append(
             {
+                "title": title,
                 "achievement_html": achievement_html,
                 "starting_condition_html": starting_condition_html,
                 "requirements_html": requirements_html,
@@ -39,10 +60,7 @@ if __name__ == "__main__":
     from eu4_achievements_manager.get_eu4_wiki import load_eu4_wiki_html
 
     html = load_eu4_wiki_html()
-    soup = BeautifulSoup(html, "html.parser")
-    table = soup.find("table")
-
-    achievements = parse_achievement_table(table)
+    achievements = parse_wiki_achievement_table(html)
 
     for achievement in achievements:
         print(achievement)
